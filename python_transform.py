7a128c8b4deeb49497aedaa68f0a43e6c5a6e188
Valay Dave <valaygaurang@gmail.com>
2020-05-18 20:02:36 -0700

    Made stable training Script and changed requirements.

--- python_transform.py
+++ python_transform.py
@@ -6,40 +6,44 @@ import numpy as np
 import os  
 import random 
 from transformers import ( 
-    GPT2Tokenizer, 
-    GPT2LMHeadModel, 
-    CONFIG_MAPPING, 
-    MODEL_WITH_LM_HEAD_MAPPING, 
-    AutoConfig, 
-    AutoModelWithLMHead, 
-    AutoTokenizer, 
-    DataCollatorForLanguageModeling, 
-    get_linear_schedule_with_warmup, 
-    HfArgumentParser, 
-    LineByLineTextDataset, 
-    PreTrainedTokenizer, 
-    TextDataset, 
-    Trainer, 
-    TrainingArguments, 
-    set_seed, 
     WEIGHTS_NAME, 
-    CONFIG_NAME
+    CONFIG_NAME,
+    PreTrainedTokenizer
 )   
 import time
+import datetime
+import click 
+import json
+
 output_dir = "./storage/models/"+str(int(time.time()))+"/"
+
 def safe_mkdir(dir_path):
     try:
         os.makedirs(dir_path)
     except:
         pass
 
-def checkpoint_model(ml_model,dir_path):
+def checkpoint_model(ml_model,tokenizer,dir_path):
     safe_mkdir(dir_path)
     output_model_file = os.path.join(dir_path, WEIGHTS_NAME)
     output_config_file = os.path.join(dir_path, CONFIG_NAME)
     torch.save(ml_model.state_dict(), output_model_file)
-    model.config.to_json_file(output_config_file)
+    ml_model.config.to_json_file(output_config_file)
     tokenizer.save_vocabulary(dir_path)
+
+def save_training_params(batch_size,epochs,lr,warmup,checkpoint,samples,blocksize,dir_path):
+    saved_data = dict(
+                    batch_size =batch_size,
+                    epochs=epochs,
+                    lr =lr,
+                    warmup =warmup,
+                    checkpoint=checkpoint,
+                    samples=samples,
+                    blocksize=blocksize,
+                    date=datetime.datetime.now().strftime("%d-%b-%Y (%H:%M:%S.%f)"))
+    safe_mkdir(dir_path)
+    with open(os.path.join(dir_path,'params.json'),'w') as outfile:
+        json.dump(saved_data,outfile)
 
 class ProgrammingLanguagesDataset(Dataset):
     def __init__(self,\
@@ -51,6 +55,7 @@ class ProgrammingLanguagesDataset(Dataset):
         with open(filenames_path) as f: 
             data = f.read()  
         python_files = data.split('\n')
+        print("Reading Samples : ",samples)
         python_files = random.sample(python_files,samples) 
         saved_data = [] 
         self.examples = []
@@ -74,64 +79,97 @@ class ProgrammingLanguagesDataset(Dataset):
     def __getitem__(self, item):
         return torch.tensor(self.examples[item], dtype=torch.long)
 
+@click.command(help='Train GPT-2 on Python Programs')
+@click.option('--batch_size',default=20,help='Batch Size of Model')
+@click.option('--epochs',default=2,help='Epoch of Model')
+@click.option('--lr',default=0.00002,help='Learning Rate')
+@click.option('--warmup',default=10000,help='Warmup Steps')
+@click.option('--checkpoint',default=True,is_flag=True)
+@click.option('--samples',default=10,help='Number of Samples From Dataset')
+@click.option('--blocksize',default=512,help='Block Size')
+def train(
+    batch_size = 20,\
+    epochs = 10,\
+    lr = 0.00002,\
+    warmup = 10000,\
+    checkpoint=True,\
+    samples=10,
+    blocksize=512
+    ):
+    from transformers import ( 
+        GPT2Tokenizer, 
+        GPT2LMHeadModel, 
+        CONFIG_MAPPING, 
+        MODEL_WITH_LM_HEAD_MAPPING, 
+        AutoConfig, 
+        AutoModelWithLMHead, 
+        AutoTokenizer, 
+        DataCollatorForLanguageModeling, 
+        get_linear_schedule_with_warmup, 
+        HfArgumentParser, 
+        LineByLineTextDataset, 
+        PreTrainedTokenizer, 
+        TextDataset, 
+        Trainer, 
+        TrainingArguments, 
+        set_seed, 
+        WEIGHTS_NAME, 
+        CONFIG_NAME
+    ) 
+    print("Models Will Be Checkpointed : ",checkpoint," Saving Training Params")
+    save_training_params(batch_size,epochs,lr,warmup,checkpoint,samples,blocksize,output_dir)
+    tokenizer = GPT2Tokenizer.from_pretrained('gpt2') 
+    model = GPT2LMHeadModel.from_pretrained('gpt2') 
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu") 
+    model = model.to(device)
+    model.train()
+    optimizer = AdamW(model.parameters(), lr=lr)
+    scheduler = get_linear_schedule_with_warmup(optimizer, num_warmup_steps=warmup, num_training_steps=-1)
+    script_count = 0
+    sum_loss = 0.0
+    batch_count = 0
+    language_dataset = ProgrammingLanguagesDataset(tokenizer=tokenizer,samples=samples,block_size=blocksize)
+    script_loader = DataLoader(language_dataset)
+    for epoch in range(epochs):
+        if epoch % 2 == 0 and epoch != 0 and checkpoint:
+            checkpoint_model(model,tokenizer,output_dir+str(epoch))
+        
+        print("Starting Epoch : ",epoch)
+        for _ , script in enumerate(script_loader):
+            outputs = model(script.to(device), labels=script.to(device))
+            loss, logits = outputs[:2]                        
+            loss.backward()
+            sum_loss = sum_loss + loss.detach().data
+                        
+            script_count = script_count + 1
+            if script_count == batch_size:
+                script_count = 0    
+                batch_count += 1
+                optimizer.step()
+                scheduler.step() 
+                optimizer.zero_grad()
+                model.zero_grad()
+                
+            if batch_count == 200:
+                model.eval()
+                print(f"sum loss {sum_loss}")
+                sample_outputs = model.generate(
+                                        bos_token_id=random.randint(1,30000),
+                                        do_sample=True,   
+                                        top_k=50, 
+                                        max_length =200,
+                                        top_p=0.95, 
+                                        num_return_sequences=1
+                                    )
 
-tokenizer = GPT2Tokenizer.from_pretrained('gpt2') 
-model = GPT2LMHeadModel.from_pretrained('gpt2') 
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu") 
-model = model.to(device) 
+                print("Output:\n" + 100 * '-')
+                for i, sample_output in enumerate(sample_outputs):
+                    print("{}: {}".format(i, tokenizer.decode(sample_output, skip_special_tokens=True)))
+                
+                batch_count = 0
+                sum_loss = 0.0
+                model.train()
+    checkpoint_model(model,tokenizer,output_dir+str(epochs))
 
-BATCH_SIZE = 20
-EPOCHS = 10
-LEARNING_RATE = 0.00002
-WARMUP_STEPS = 10000
-
-model = model.to(device)
-model.train()
-optimizer = AdamW(model.parameters(), lr=LEARNING_RATE)
-scheduler = get_linear_schedule_with_warmup(optimizer, num_warmup_steps=WARMUP_STEPS, num_training_steps=-1)
-script_count = 0
-sum_loss = 0.0
-batch_count = 0
-
-script_loader = DataLoader(ProgrammingLanguagesDataset(tokenizer=tokenizer))
-for epoch in range(EPOCHS):
-    if epoch % 2 == 0 and epoch != 0:
-        checkpoint_model(model,output_dir+str(epoch))
-    
-    print("Starting Epoch : ",epoch)
-    for _ , script in enumerate(script_loader):
-        outputs = model(script.to(device), labels=script.to(device))
-        loss, logits = outputs[:2]                        
-        loss.backward()
-        sum_loss = sum_loss + loss.detach().data
-                       
-        script_count = script_count + 1
-        if script_count == BATCH_SIZE:
-            script_count = 0    
-            batch_count += 1
-            optimizer.step()
-            scheduler.step() 
-            optimizer.zero_grad()
-            model.zero_grad()
-            
-        if batch_count == 200:
-            model.eval()
-            print(f"sum loss {sum_loss}")
-            sample_outputs = model.generate(
-                                    bos_token_id=random.randint(1,30000),
-                                    do_sample=True,   
-                                    top_k=50, 
-                                    max_length =200,
-                                    top_p=0.95, 
-                                    num_return_sequences=1
-                                )
-
-            print("Output:\n" + 100 * '-')
-            for i, sample_output in enumerate(sample_outputs):
-                  print("{}: {}".format(i, tokenizer.decode(sample_output, skip_special_tokens=True)))
-            
-            batch_count = 0
-            sum_loss = 0.0
-            model.train()
-
-checkpoint_model(model,output_dir+str(EPOCHS))
+if __name__ == "__main__":
+    train()
