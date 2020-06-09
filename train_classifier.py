97896e1a7691dc8cf7de2b5cec85e12e1a8263ed
Valay Dave <valaygaurang@gmail.com>
2020-06-08 18:55:53 -0700

    possible bug fix.

--- train_classifier.py
+++ train_classifier.py
@@ -151,7 +151,7 @@ def accuracy(output, target, topk=(1,),conf_matrix=None):
         #Add to confusion matrix if supplied.
         if conf_matrix is not None:
             pred_argmax = torch.argmax(pred,dim=1)
-            conf_matrix.add_batch(pred_argmax,target)
+            conf_matrix.add_batch(pred_argmax.numpy(),target.numpy())
 
         correct = pred.eq(target.view(1, -1).expand_as(pred))
         res = []
